a54e47d1e6f72b21eb7a70459e21bb7826a67218
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-08-02 03:06:04 +0530

    added Prisma ORM for Joining Request

--- src/app/api/join/route.ts
+++ src/app/api/join/route.ts
@@ -1,8 +1,23 @@
 import { NextResponse } from 'next/server'
- 
+import { PrismaClient } from '@prisma/client'
+
 export async function POST(request: Request) {
+  const prisma = new PrismaClient();
+
   const res = await request.json()
 
-  
-  return NextResponse.json(res)
+  try {
+    const client = {
+      name: res.name,
+      email: res.email,
+      contact: res.contact
+    }
+    await prisma.client.create({
+      data: client
+    })
+    return NextResponse.json({ response: true })
+  }
+  catch {
+    return NextResponse.json({ response: true })
+  }
 }
